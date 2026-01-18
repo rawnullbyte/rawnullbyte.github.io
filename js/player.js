c5f0844d713e559165b908d58f11d01037b6c050
rawnullbyte <andrexyt2012@gmail.com>
2026-01-18 19:10:34 +0100

    .

--- js/player.js
+++ js/player.js
@@ -16,19 +16,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Playlist
     const playlist = [
-        'songs/losing contact 道 - I want to stop having nightmares when I go to bed..mp3',
-        'songs/stillveil, Mempty - Please forget me.mp3',
-        'songs/lolipushing - choker.mp3',
-        'songs/Ashen Blood - a distant memory (ft. sovietico).mp3',
-        'songs/Ashen Blood - as i watch everything fall.mp3',
-        'songs/Ashen Blood - losing myself.mp3',
-        'songs/Ashen Blood - nothing feels real anymore.mp3',
-        'songs/Ashen Blood - please... be quiet.mp3',
+        "songs/losing contact 道 - I want to stop having nightmares when I go to bed..mp3",
+        "songs/stillveil - I'm tired of my life.mp3",
+        "songs/stillveil, Mempty - Please forget me.mp3",
+        "songs/lolipushing - choker.mp3",
+        "songs/Ashen Blood - a distant memory (ft. sovietico).mp3",
+        "songs/Ashen Blood - as i watch everything fall.mp3",
+        "songs/Ashen Blood - losing myself.mp3",
+        "songs/Ashen Blood - nothing feels real anymore.mp3",
+        "songs/Ashen Blood - please... be quiet.mp3",
         "songs/i don't really feel like eating anymore - for you.mp3",
-        'songs/i want to throw up - i want to throw up.mp3',
-        'songs/Loli in early 20s - Da Da Is Tape To And U U U.mp3',
-        'songs/cyygnil - 081125.mp3',
-        'songs/redeath - i need you.mp3'
+        "songs/i want to throw up - i want to throw up.mp3",
+        "songs/Loli in early 20s - Da Da Is Tape To And U U U.mp3",
+        "songs/cyygnil - 081125.mp3",
+        "songs/redeath - i need you.mp3"
     ];
 
     let currentIndex = 0;
