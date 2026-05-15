07d046c773e2656f352fccd815821a4d7e6ae41d
rawnullbyte <andrexyt2012@gmail.com>
2026-05-15 23:02:07 +0200

    Update counter.

--- src/components/ViewCounter.jsx
+++ src/components/ViewCounter.jsx
@@ -2,7 +2,7 @@ export default function ViewCounter() {
   return (
     <div className="fixed bottom-5 right-5 z-[2]">
       <img
-        src="https://count.getloli.com/@rawnullbyte-web?name=Profile+Views&theme=green&padding=7&offset=0&align=center&scale=0.6&pixelated=1&darkmode=auto"
+        src="https://count.getloli.com/@rawnullbyte?name=Profile+Views&theme=green&padding=7&offset=0&align=center&scale=0.6&pixelated=1&darkmode=auto"
         alt="Profile views"
       />
     </div>
